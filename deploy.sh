b4d042b5ad7f98fa92a837b8fc2172741bdf1d50
Vivek Singh <vivekkmr45@yahoo.in>
2018-07-09 22:17:39 +0530

    Added conditional check for repo clone
    Signed-off-by: Vivek Singh <vivekkmr45@yahoo.in>

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,23 @@
 # Go one step back from the current directory
 cd ..
 
-# Clone OpenFaaS repository 
-echo "Cloning OpenFaaS repository"
-git clone https://github.com/openfaas/faas
 
-# Move to faas directory
-cd faas
+if [ -d "faas" ]; 
+then
+    echo "Repository already exists"
+    echo "Updating with origin"
+    cd faas
+    git pull origin master
+    echo ""
+else 
+    # Clone OpenFaaS repository 
+    echo "Cloning OpenFaaS repository"
+    git clone https://github.com/openfaas/faas
+    echo "Cloned"
+    echo ""
+    # Move to faas directory
+    cd faas
+fi
 
 # Deploy the stack
 echo "Deploying the stack"
